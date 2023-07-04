f13c52494012d6f76c0435a32e219e74da156b3c
aperkes <perkes.ammon@gmail.com>
2023-07-04 16:07:29 -0700

    fixed a bug and updated the info files

--- get_info.sh
+++ get_info.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
-echo Gene info > A_gene_info.txt
+infile="A_genes.csv"
+outfile="A_gene_info.txt"
 
-for g in 'cat A_genes.csv'; do
-    zgrep -h $g /data/sequencing/MollyGenome/GCF_000485575.1_Poecilia_formosa-5.1.2_rna.fna.gz >> A_gene_info.txt
-    done
+echo "Gene info" > $outfile
+
+while read -r g; do
+    zgrep -h $g /data/sequencing/MollyGenome/GCF_000485575.1_Poecilia_formosa-5.1.2_rna.fna.gz >> $outfile
+    done <$infile
 
