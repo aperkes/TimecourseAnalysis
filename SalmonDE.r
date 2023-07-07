66a296a3e1f96bb6a95d2a22238680cfc8c5c6ed
aperkes <perkes.ammon@gmail.com>
2023-07-07 16:24:39 -0700

    almost there...

--- SalmonDE.r
+++ SalmonDE.r
@@ -12,14 +12,27 @@
 
 library('DESeq2')
 library('tximport')
+library('jsonlite')
 
 samples <- read.csv('~/Documents/Scripts/TimecourseAnalysis/SampleInfo.csv',header=TRUE)
 rownames(samples) <- samples$SampleName
 head(samples[,c("Treatment",'Time','Batch')])
 
-files <-file.path('/data/sequencing/TimeFights/results/Salmon_quant/',samples$SampleName,'.quant')
+files <-file.path('/data/sequencing/TimeFights/results/Salmon_quant',samples$SampleName)
+files <- paste(sep="",files,".quant/quant.sf")
 names(files) <- samples$SampleName
 
+head(files)
 #tx2gene <- read_csv(tx2gene.gencode.csv)
 #txi <- tximport(files,type= "salmon",tx2gene=tx2gene)
+txi <- tximport(files,type= "salmon",txOut = TRUE)
 
+ddsTxi <- DESeqDataSetFromTximport(txi,colData = samples,design = ~ Treatment + Time + Batch)
+dds <- DESeq(ddsTxi)
+res <- results(dds,contrast = c("Treatment","W","L")
+
+head(res)
+resOrdered <- res[order(res$padj),]
+head(res[order(res$padj),])
+
+sum(res$padj<0.05,na.rm=TRUE)
